688726c13563f9b07ef48be08c9e1cc9f2ee45c1
Emory Merryman <emory.merryman@gmail.com>
2016-06-07 09:35:54 +0000

    correct path error

--- sbin/init.sh
+++ sbin/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH=${PATH}:/opt/protonshiny/bin &&
+export PATH=${PATH}:/opt/protonshiny/sbin &&
     init-ssh.sh &&
     su --login emory /opt/protonshiny/bin/init.sh &&
     true
