a85d7d18df7dd250dcc31f1b6f125653058b50c5
Emory Merryman <emory.merryman@gmail.com>
2016-06-07 01:18:34 +0000

    replace greenlobster

--- bin/init.sh
+++ bin/init.sh
@@ -2,5 +2,5 @@
 
 export PATH=${PATH}:/opt/protonshiny/bin &&
     init-ssh.sh &&
-    node /opt/greenlobster/c9sdk/server.js --listen 0.0.0.0 -w ${HOME}/workspace -p 8080 --auth ${USER}:${USER} 
+    node /opt/protonshiny/c9sdk/server.js --listen 0.0.0.0 -w ${HOME}/workspace -p 8080 --auth ${USER}:${USER} 
     true
--- sbin/install-cloud9.sh
+++ sbin/install-cloud9.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 dnf install --assumeyes git make python tar which bzip2 ncurses gmp-devel mpfr-devel libmpc-devel glibc-devel flex bison glibc-static zlib-devel gcc gcc-c++ nodejs curl bash &&
-    mkdir /opt/greenlobster/c9sdk &&
-    chmod 0555 /opt/greenlobster/c9sdk &&
-    git -C /opt/greenlobster/c9sdk init &&
-    git -C /opt/greenlobster/c9sdk remote add origin git://github.com/c9/core.git &&
-    git -C /opt/greenlobster/c9sdk pull origin master &&
-    /opt/greenlobster/c9sdk/scripts/install-sdk.sh &&
+    mkdir /opt/protonshiny/c9sdk &&
+    chmod 0555 /opt/protonshiny/c9sdk &&
+    git -C /opt/protonshiny/c9sdk init &&
+    git -C /opt/protonshiny/c9sdk remote add origin git://github.com/c9/core.git &&
+    git -C /opt/protonshiny/c9sdk pull origin master &&
+    /opt/protonshiny/c9sdk/scripts/install-sdk.sh &&
     true
