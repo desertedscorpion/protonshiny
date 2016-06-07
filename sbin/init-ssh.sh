add7455354ee70ad4189e36b0a05f2c0f0e69a30
Emory Merryman <emory.merryman@gmail.com>
2016-06-07 11:42:27 +0000

    mispelling

--- sbin/init-ssh.sh
+++ sbin/init-ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir /opt/protonshiny/ssh &&
-    cp /opt/protonshiny/private/ssh/id_rsa  /opt/protonshiny/ssh &&
+    cp /opt/protonshiny/private/ssh/id_rsa /opt/protonshiny/ssh/id_rsa &&
     chmod a+r /opt/protonyshiny/ssh/id_rsa &&
     true
