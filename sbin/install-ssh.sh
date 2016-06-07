c7d29c9ace159a5a335e211259595cef4744c42c
Emory Merryman <emory.merryman@gmail.com>
2016-06-07 01:36:04 +0000

    fix openssh-clients

--- sbin/install-ssh.sh
+++ sbin/install-ssh.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 dnf install --assumeyes openssh-clients &&
+    cp /opt/protonshiny/private/ssh/id_rsa /opt/protonshiny/ssh/id_rsa &&
+    cp /opt/protonshiny/private/ssh/config /opt/protonshiny/ssh/config &&
+    chmod a+r /opt/protonyshiny/ssh/id_rsa /opt/protonshiny/ssh/config &&
     true
