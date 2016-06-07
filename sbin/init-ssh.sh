6c96790ac7320d29553ab2529398e815e0bbb871
Emory Merryman <emory.merryman@gmail.com>
2016-06-07 11:25:01 +0000

    We don't really need ~/.ssh/config

--- sbin/init-ssh.sh
+++ sbin/init-ssh.sh
@@ -2,6 +2,5 @@
 
 mkdir /opt/protonshiny/ssh &&
     cp /opt/protonshiny/private/ssh/id_rsa  /opt/protonshiny/ssh &&
-    cp /opt/protonshiny/private/ssh/config /opt/protonshiny/ssh &&
-    chmod a+r /opt/protonyshiny/ssh/id_rsa /opt/protonshiny/ssh/config &&
+    chmod a+r /opt/protonyshiny/ssh/id_rsa &&
     true
