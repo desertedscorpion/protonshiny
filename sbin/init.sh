71a01fa1745fc1d139b8e1cd59ef1cdab933c268
Emory Merryman <emory.merryman@gmail.com>
2016-06-07 08:58:17 +0000

    export PATH

--- sbin/init.sh
+++ sbin/init.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-( [ ! -d /opt/protonshiny/init/scripts/sbin ] ||
-    for SCRIPT in /opt/protonshiny/inti/scripts/sbin/*
-    do
-        bash ${SCRIPT} &&
-        true
-    done &&
-    true
-) &&
+export PATH=${PATH}:/opt/protonshiny/bin &&
     init-ssh.sh &&
     su --login emory /opt/protonshiny/bin/init.sh &&
     true
