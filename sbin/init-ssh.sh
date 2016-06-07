1f8e3478458b59da4087fe2aa90cbbdbfc8bc38e
Emory Merryman <emory.merryman@gmail.com>
2016-06-07 09:17:25 +0000

    mkdir

--- sbin/init-ssh.sh
+++ sbin/init-ssh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cp /opt/protonshiny/private/ssh/id_rsa /opt/protonshiny/private/ssh/config /opt/protonshiny/ssh &&
+mkdir /opt/protonshiny/ssh &&
+    cp /opt/protonshiny/private/ssh/id_rsa /opt/protonshiny/private/ssh/config /opt/protonshiny/ssh &&
     chmod a+r /opt/protonyshiny/ssh/id_rsa /opt/protonshiny/ssh/config &&
     true
