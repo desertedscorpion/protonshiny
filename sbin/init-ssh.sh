1072fba0bd5ef0da5784f8685240b463a25b17b6
Emory Merryman <emory.merryman@gmail.com>
2016-06-07 01:46:16 +0000

    fix install-ssh

---
+++ sbin/init-ssh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp /opt/protonshiny/private/ssh/id_rsa /opt/protonshiny/private/ssh/config /opt/protonshiny/ssh &&
+    chmod a+r /opt/protonyshiny/ssh/id_rsa /opt/protonshiny/ssh/config &&
+    true
